83601ed0f7c376e54fa9d160cc7242ffe18eaabb
Joel <franco4prez@gmail.com>
2022-08-05 13:06:32 -0400

    completed TODO tasks

--- Variables Exercise 01/Program.cs
+++ Variables Exercise 01/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System.Collections.Generic;
+using System;
 
 namespace Variables_Exercise_01
 {
@@ -10,6 +11,10 @@ namespace Variables_Exercise_01
             // - Once finished save, commit, and then push to GitHub!
 
             // TODO: Declare 3 constansts
+
+            const int myAge = 24;
+            const double price = 24.99D;
+            const string myName = "Joel";
 
             #region Types
             // string
@@ -23,9 +28,24 @@ namespace Variables_Exercise_01
             // TODO: Declare a variable for each of the types above
             // Make sure to use C# naming convention best practices 
 
+            string myPet = "Decker";
+            char myFavoriteLetter = 'F';
+            bool favoritePizza = true;
+            int catAge = 115;
+            double price2 = 15.99D;
+            decimal myGDP = 1.234431M;
+
+
             // TODO: Initialize each of the variables with a value
 
-            // TODO: Write out each of the variables to the Console 
+            // TODO: Write out each of the variables to the Console
+
+            Console.WriteLine(myPet);
+            Console.WriteLine(myFavoriteLetter);
+            Console.WriteLine(favoritePizza);
+            Console.WriteLine(catAge);
+            Console.WriteLine(price2);
+            Console.WriteLine(myGDP);
         }
 
     }
